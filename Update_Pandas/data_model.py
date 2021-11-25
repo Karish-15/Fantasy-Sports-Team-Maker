f408e4cc857631ae41896676f1ad5d0673954d8a
Karish <karish.ch15@gmail.com>
2021-11-26 01:31:56 +0530

    Added support for CSV files

---
+++ Update_Pandas/data_model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+
+class Database:
+    df = pd.read_csv('sample.csv')
+    df.fillna(0, inplace=True)
+
+    df.set_index("Name", inplace=True)
+
+    @staticmethod
+    def get_by_name(player, col):
+        return Database.df[col][player]
+    
+    @classmethod
+    def save_csv(cls, filename):
+        cls.df.reset_index(inplace=True)
+        cls.df.to_csv(filename, index=False)
+
+class Player:
+    def __init__(self, name, position, team) -> None:
+        self.name = name
+        self.position = position
+        self.team = team
+
+        self.standard_deviation = float(Database.df.loc[self.name, 'STANDARD'])
+        self.mean = float(Database.df.loc[self.name, 'MEAN'])
+
+    def get_total(self):
+        return Database.df['total'][self.name]
+
+if __name__ == '__main__':
+    player = Player('RAHUL', 'BOWL', 'A')
+
+    print(player.standard_deviation)
+
+
+
+
+
+
+
+
---
+++ Update_Pandas/set_csv.py
@@ -0,0 +1,48 @@
+from data_model import Database, Player
+import pandas as pd
+import statistics
+
+#Return Row
+# print(Database.df.loc(['BUMRAH']))
+
+
+
+for index, row in Database.df.iterrows():
+    # index will be player name here, row is preceding columns
+
+    sum_values = 0 # Sum of player's performance values
+    track_column = 0 # Keep track of column number
+    matches_played = 0 # Number of matches played
+    player_values = []
+
+    for index_row, value in row.iteritems():
+        if track_column == 8:
+            break
+        else:
+            if(value != 0):
+                matches_played += 1
+                player_values.append(int(value))
+            track_column += 1
+            # player_values.append(int(value))
+
+    sum_values = sum(player_values)
+    mean = float(sum_values)/float(matches_played) # Mean of performance points for current player
+
+    standard_deviation = statistics.pstdev(player_values) # Calculate standard deviation    
+
+    median = statistics.median(player_values)
+
+    # Update values in Dataframe
+    Database.df.loc[index, 'MEAN'] = mean
+    Database.df.loc[index, 'STANDARD'] = standard_deviation
+    Database.df.loc[index, 'MEDIAN'] = median
+    
+
+if __name__ == '__main__':
+    print(Database.df)
+    Database.save_csv('sample.csv')
+
+
+
+
+    
